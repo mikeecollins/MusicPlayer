61f3de22001e7971d2b086a21b8a684fc19e59a7
Your Name <you@example.com>
2025-03-26 15:25:37 +0000

    Updated both List Activities for Music and Artist added main menu

--- app/src/main/java/models/ArtistModel.kt
+++ app/src/main/java/models/ArtistModel.kt
@@ -4,5 +4,5 @@ data class ArtistModel(var artistname: String ="",
                        var socialmedia: String ="",
                        var artistTitle: String = "",
                        var age: Int = 0,
-                       var dateofbirth: Int = 0,
+                       var dateofbirth: String = "",
                        var aboutartist: String = "")
